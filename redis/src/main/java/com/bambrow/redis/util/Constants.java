2cf6b4f2a5e928a6964e37d50af0a518a208066e
bambrow <bambrow@foxmail.com>
2021-06-23 02:49:16 +0000

    optimize imports

--- redis/src/main/java/com/bambrow/redis/util/Constants.java
+++ redis/src/main/java/com/bambrow/redis/util/Constants.java
@@ -3,11 +3,6 @@ package com.bambrow.redis.util;
 public class Constants {
 
     public static final String STUDENT_ID = "student_id";
-    public static final String STUDENT_NAME = "student_name";
-    public static final String STUDENT_USERNAME = "student_username";
-
     public static final String S_ID_REDIS = STUDENT_ID + "::";
-    public static final String S_NAME_REDIS = STUDENT_NAME + "::";
-    public static final String S_USERNAME_REDIS = STUDENT_USERNAME + "::";
 
 }
